5e0478d3fbc7f0cf5d84679188610a31bd6536d1
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-11-05 11:41:01 -0600

    conforms to protocol, updated intializers for sliders

--- CustomControls/ViewController.swift
+++ CustomControls/ViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CustomSliderDelegate {
     
-    let customSlider = CustomSlider(frame:CGRectZero, componentID:0)
-    let customSlider1 = CustomSlider(frame:CGRectZero, componentID:1)
-    let customSlider2 = CustomSlider(frame:CGRectZero, componentID:2)
+    
+    var customSlider:CustomSlider!
+    var customSlider1:CustomSlider!
+    var customSlider2:CustomSlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        customSlider = CustomSlider(frame:CGRectZero, componentID:0, delegate:self)
+        customSlider1 = CustomSlider(frame:CGRectZero, componentID:1, delegate:self)
+        customSlider2 = CustomSlider(frame:CGRectZero, componentID:2, delegate:self)
+
         view.addSubview(customSlider)
         view.addSubview(customSlider1)
         view.addSubview(customSlider2)
@@ -32,11 +38,19 @@ class ViewController: UIViewController {
         let margin:CGFloat = 20.0
         let width = view.bounds.width - 2*margin
         customSlider.frame = CGRect(x: margin, y: margin+topLayoutGuide.length, width: width, height: 50.0)
-        
         customSlider1.frame = CGRect(x: margin, y: margin+topLayoutGuide.length + 100, width: width, height: 50.0)
-        
         customSlider2.frame = CGRect(x: margin, y: margin+topLayoutGuide.length + 200, width: width, height: 50.0)
-
+    }
+    
+    func update(fromComponent:Int, color:CGColor){
+        for subview in view.subviews{
+            if let slider = subview as? CustomSlider{
+                if slider.componentID != fromComponent{
+                    slider.thumbLayer.backgroundColor = color
+                    slider.mainColorComponents = color.getComponents()
+                }
+            }
+        }
     }
 
 }
